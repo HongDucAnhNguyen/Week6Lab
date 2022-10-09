50e0b9e261c9e697dcf7415ccb00a009084268cd
Jay Nguyen <bobguy0606@gmail.com>
2022-10-09 16:31:27 -0700

    passed all tests

--- build/web/WEB-INF/jsp/register.jsp
+++ build/web/WEB-INF/jsp/register.jsp
@@ -10,15 +10,17 @@
 <!DOCTYPE html>
 <html>
     <head>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
         <title>Register Page</title>
     </head>
-    <body>
+    <body style="text-align: center">
         <h1>Shopping List</h1>
         <form method="POST" action="ShoppingList">
             <label>Username: </label>
             <input type="text" name="username" required>
             <input type="submit" value="Register name" >
+            <input type="hidden" name="action" value="register">
         </form>
     </body>
 </html>
--- src/java/servlet/ShoppingList.java
+++ src/java/servlet/ShoppingList.java
@@ -24,32 +24,64 @@ public class ShoppingList extends HttpServlet {
     @Override
     public void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
         HttpSession session = req.getSession();
-        
-        if (req.getParameter("logout") != null) {
+        String action = req.getParameter("action");
+        //if user wants to logout
+        if (action != null && action.equals("logout")) {
+            //invalidates session and forward back to register page
+            items.clear();
             session.invalidate();
             getServletContext().getRequestDispatcher("/WEB-INF/jsp/register.jsp").forward(req, res);
-            res.sendRedirect("register");
+            return;
         }
-        if (session != null) {
+
+        if (session.getAttribute("username") != null) {
             getServletContext().getRequestDispatcher("/WEB-INF/jsp/shoppingList.jsp").forward(req, res);
-            res.sendRedirect("shoppingList");
         } else {
             getServletContext().getRequestDispatcher("/WEB-INF/jsp/register.jsp").forward(req, res);
         }
+
     }
 
     @Override
     public void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+        
+       
 
         HttpSession session = req.getSession();
         String username = req.getParameter("username");
+        String action = req.getParameter("action");
         String item = req.getParameter("item");
-        session.setAttribute("username", username);
-        items.add(item);
-        session.setAttribute("itemList", items);
+        String[] userItems = req.getParameterValues("userItems");
+        
+        try{
+        if (action.equals("register")) {
+            session.setAttribute("username", username);
 
+        } else if (action.equals("add")) {
+            items.add(item);
+            session.setAttribute("itemList", items);
+        }
+        else if (action.equals("delete")) {
+            for (int i = 0; i < userItems.length; i++) {
+                for(int j = 0; j < items.size(); j++){
+                    if (userItems[i].equals(items.get(j))) {
+                    items.remove(j);
+                    session.setAttribute("itemList", items);
+
+                }
+                }
+                
+            }
+        }
         getServletContext().getRequestDispatcher("/WEB-INF/jsp/shoppingList.jsp").forward(req, res);
+        }
+         catch(Exception e){
+            req.setAttribute("message", "no items to delete");
+            getServletContext().getRequestDispatcher("/WEB-INF/jsp/shoppingList.jsp").forward(req, res);
+        }
 
     }
+
+    
 
 }
